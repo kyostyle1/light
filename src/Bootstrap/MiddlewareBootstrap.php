443d7affe4988f9c04d71c8543fadb524ef536ad
NhatBangDinh <dinhnhatbang@gmail.com>
2017-09-14 23:22:26 +0700

    Add cors middleware

--- src/Bootstrap/MiddlewareBootstrap.php
+++ src/Bootstrap/MiddlewareBootstrap.php
@@ -1,15 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BangDinh
- * Date: 9/14/17
- * Time: 23:15
- */
 
 namespace Cottect\Light\Bootstrap;
 
+use Cottect\Light\Api;
+use Cottect\Light\BootstrapInterface;
+use Cottect\Light\Middleware\UrlQueryMiddleware;
+use Cottect\Light\Middleware\FractalMiddleware;
+use Cottect\Light\Middleware\CorsMiddleware;
+use Cottect\Light\Middleware\NotFoundMiddleware;
+use Cottect\Light\Middleware\OptionsResponseMiddleware;
 
-class MiddlewareBootstrap
+use Phalcon\Config;
+use Phalcon\DiInterface;
+
+class MiddlewareBootstrap implements BootstrapInterface
 {
-
+    public function run(Api $api, DiInterface $di, Config $config)
+    {
+        $api
+            ->attach(new CorsMiddleware($config->get('cors')->allowedOrigins->toArray()))
+            ->attach(new OptionsResponseMiddleware)
+            ->attach(new NotFoundMiddleware)
+            ->attach(new FractalMiddleware)
+            ->attach(new UrlQueryMiddleware);
+    }
 }
